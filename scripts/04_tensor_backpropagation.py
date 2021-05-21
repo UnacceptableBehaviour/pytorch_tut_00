420fdc68a46d2049dff6a8a954c1378463674038
UnacceptableBehaviour <fernandez.simon@gmail.com>
2021-05-21 18:26:26 +0100

    04 code

--- scripts/04_tensor_backpropagation.py
+++ scripts/04_tensor_backpropagation.py
@@ -11,9 +11,37 @@ print("-" * 80)
 print("\n" * 2)
 
 
+# 0m - intro to gradients
+# 1m - theory - chain rule
+# 2m - computational graph
+# 4m12 - backward pass - concept overview
+# 4m30 - linear regression
+# 4m50 -  walk through the maths
+# 10m30 - pytorch implementation
+
+# y hat - predicted loss
+
+x = torch.tensor(1.0)
+print(f"\n torch.tensor(1.0) \n{ x }")
+y = torch.tensor(2.0)
+print(f"\n torch.tensor(2.0) \n{ y }")
+
+w = torch.tensor(1.0, requires_grad=True)
+print(f"\n w = torch.tensor(1.0, requires_grad=True) \n{ w }")
 
 
-#print(f"\n  \n{  }")
+y_hat = w * x
+print(f"\n y_hat = w * x \n{ y_hat }")
+
+loss = (y_hat - y)**2
+print(f"\n loss = (y_hat - y)**2 \n{ loss }")
+
+print('- - - - - backward pass')
+loss.backward()
+print(f"\n loss.backward() (w.grad)\n{ w.grad }")
+
+# update weights
+# next forward pass & back prpagate . . . loop
 
 #print(f"\n  \n{  }")
 print('\n')
---
+++ scripts/05_tensor_model_optimisation.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+# PyTorch Tutorial 03 - Gradient Calculation With Autograd
+# https://www.youtube.com/watch?v=DbeIqrwb_dE&list=PLqnslRFeH2UrcDBWF5mfPGpqQDSta6VK4&index=3
+
+#from __future__ import print_function
+import torch
+print("\n" * 20)
+print("-" * 80)
+print("-" * 80)
+print("\n" * 2)
+
+
+
+
+#print(f"\n  \n{  }")
+
+#print(f"\n  \n{  }")
+print('\n')
