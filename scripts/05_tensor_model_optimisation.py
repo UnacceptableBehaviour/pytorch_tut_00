8a390c860c1c32d77a7c4130f06543174e4029b6
UnacceptableBehaviour <fernandez.simon@gmail.com>
2021-05-21 19:24:46 +0100

    05 hand crafted

--- scripts/05_tensor_model_optimisation.py
+++ scripts/05_tensor_model_optimisation.py
@@ -5,13 +5,72 @@
 
 #from __future__ import print_function
 import torch
+import numpy as np
+
 print("\n" * 20)
 print("-" * 80)
 print("-" * 80)
 print("\n" * 2)
 
 
+# 0m - Manual, Prediction, Gradient Computation, Loss Computation, Parameter updates
+# 12m10 -
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 
+# f = 2 * x
+X = np.array([1,2,3,4], dtype=np.float32)
+Y = np.array([2,4,6,8], dtype=np.float32)
+w = 0.0
+
+# model predition
+def forward(x):
+  return w * x
+
+# loss = MSE - Mean Squared Error
+def loss(y, y_predicted):
+  return ((y_predicted - y)**2).mean()
+
+# gradient
+#       mean   prediction   actual value   squared
+# MSE = 1/N *  (w*x       -    y)           **2
+# J - objective function
+# dJ/dw = 1/N 2*x (w*x - y)
+def gradient(x, y, y_predicted):
+  return np.dot(2*x, y_predicted-y).mean()
+
+print(f'prediction BEFORE training: f(5) = {forward(5):.3f}')
+
+learning_rate = 0.01
+n_iters = 20 # 10
+
+for epoch in range(n_iters):
+  # prediction = forward pass
+  y_pred = forward(X)
+
+  # loss
+  l = loss(Y, y_pred)
+
+  # gradients
+  dw = gradient(X, Y, y_pred)
+
+  # update weights   -= negative gradient direction
+  w -= learning_rate * dw
+
+  if epoch % 1 ==  0:
+    print(f'epoch {epoch+1}: w = {w:.3f}, loss = {l:.8f}')
+
+
+print(f'prediction AFTER training: f(5) = {forward(5):.3f}')
 
 #print(f"\n  \n{  }")
 
