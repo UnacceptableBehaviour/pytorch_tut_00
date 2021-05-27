f25bd219530a6878efa5f1540d891dbded800628
UnacceptableBehaviour <fernandez.simon@gmail.com>
2021-05-27 19:00:19 +0100

    13 feed forward NN

--- scripts/13_tensor_feed_froward_NN.py
+++ scripts/13_tensor_feed_froward_NN.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
-# PyTorch Tutorial 03 - Gradient Calculation With Autograd
-# https://www.youtube.com/watch?v=DbeIqrwb_dE&list=PLqnslRFeH2UrcDBWF5mfPGpqQDSta6VK4&index=3
+# PyTorch Tutorial 13 - Feed-Forward Neural Network
+# https://www.youtube.com/watch?v=oPhxf2fXHkQ&list=PLqnslRFeH2UrcDBWF5mfPGpqQDSta6VK4&index=13
 
 #from __future__ import print_function
 import torch
@@ -14,6 +14,20 @@ print("\n" * 2)
 # 1m - theory - chain rule
 # 2m - computational graph
 # etc
+
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+# MNIST
+# Dataloader, Transformation
+# Multilayer NN Activation function
+# Loss & Optimiser
+# Training Loop
+# Model Evaluation
+# GPU support
 
 
 #print(f"\n  \n{  }")
