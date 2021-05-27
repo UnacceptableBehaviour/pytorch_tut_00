d07e74194e5d26df8b91b1d5a14fb949d70c9331
UnacceptableBehaviour <fernandez.simon@gmail.com>
2021-05-27 19:12:16 +0100

    load & display data sets

--- scripts/13_tensor_feed_froward_NN.py
+++ scripts/13_tensor_feed_froward_NN.py
@@ -21,6 +21,7 @@ import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
+# 0m - Overview:
 # MNIST
 # Dataloader, Transformation
 # Multilayer NN Activation function
@@ -28,6 +29,56 @@ import matplotlib.pyplot as plt
 # Training Loop
 # Model Evaluation
 # GPU support
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"\n device \n{ device }")
+
+# Hyper-parameters
+input_size = 784 # images 28x28
+hidden_size = 500
+num_classes = 10
+num_epochs = 2
+batch_size = 100
+learning_rate = 0.001
+
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root='./data',
+                                           train=True,
+                                           transform=transforms.ToTensor(),
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='./data',
+                                          train=False,
+                                          transform=transforms.ToTensor())
+
+# Data loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
+
+examples = iter(test_loader)
+example_data, example_targets = examples.next()
+
+for i in range(6):
+    plt.subplot(2,3,i+1)
+    plt.imshow(example_data[i][0], cmap='gray')
+plt.show()
+
+#print(f"\n  \n{  }")
+
+
+# MNIST
+# Dataloader, Transformation
+# Multilayer NN Activation function
+# Loss & Optimiser
+# Training Loop
+# Model Evaluation
+# GPU support
+
 
 
 #print(f"\n  \n{  }")
