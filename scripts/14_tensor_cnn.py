06736ed33817f1bfe88a37e401b09d04da91ff8f
UnacceptableBehaviour <fernandez.simon@gmail.com>
2021-05-31 21:31:10 +0100

    14 CNN load CIFAR dataset

--- scripts/14_tensor_cnn.py
+++ scripts/14_tensor_cnn.py
@@ -33,6 +33,91 @@ print("\n" * 2)
 # test model
 
 
+import torch
+
+import torch.nn as nn       # PyTorch nn module has high-level APIs to build a neural network.
+  # Torch. nn module uses Tensors and Automatic differentiation modules for training and building layers such as input,
+  # hidden, and output layers - DOCS: https://pytorch.org/docs/stable/nn.html
+
+import torch.nn.functional as F # https://pytorch.org/docs/stable/nn.functional.html
+  # What is the difference between torch.nn and torch.nn.functional?            FROM discuss.pytorch.org
+  #
+  # While the former defines nn.Module classes, the latter uses a functional (stateless) approach.
+  #
+  # - - torch.nn - Module classes
+  # nn.Modules are defined as Python classes and have attributes, e.g. a nn.Conv2d module will
+  # have some internal attributes like self.weight.
+  #
+  # - - torch.nn.functional - function versions
+  # F.conv2d however just defines the operation and needs all arguments
+  # to be passed (including the weights and bias).
+
+  # Internally the modules will usually call their functional counterpart in the forward method somewhere.
+
+
+import torchvision
+  # popular datasets, model architectures, and common image transformations for computer vision.
+  # conda install torchvision -c pytorch    # already installed.
+
+import torchvision.transforms as transforms
+  # transforms that can be applied to Tensors - can be 'composed' into sets of transforms
+  # https://pytorch.org/vision/stable/transforms.html
+  # see 10_tensor_dataset_transforms.py &
+
+import numpy as np      # NumPy is a library for the Python programming language, adding support for large,
+  # multi-dimensional arrays and matrices, along with a large collection of high-level mathematical functions to operate
+  # on these arrays - DOCS: https://numpy.org/doc/stable/user/whatisnumpy.html
+
+import matplotlib.pyplot as plt # Matplotlib is a plotting library for the Python programming language. It provides an
+  # object-oriented API for embedding plots into applications using general-purpose GUI toolkits like Tkinter,
+  # wxPython, Qt, or GTK - DOCS:
+  # cheatsheets: https://github.com/matplotlib/cheatsheets#cheatsheets
+  # How to plot & save graph hello world: https://github.com/UnacceptableBehaviour/latex_maths#python---matplotlib-numpy
+
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyper-parameters
+num_epochs = 5
+batch_size = 4
+learning_rate = 0.001
+
+# dataset has PILImage images of range [0, 1].
+# We transform them to Tensors of normalized range [-1, 1]
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+# CIFAR10: 60000 32x32 color images in 10 classes, with 6000 images per class
+train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+
+test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
+                                          shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
+                                         shuffle=False)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# get some random training images
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))
 
 
 #print(f"\n  \n{  }")
